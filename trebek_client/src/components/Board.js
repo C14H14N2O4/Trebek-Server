682527dad455211477548070b550ec005957463d
C14H14N2O4 <99705187+C14H14N2O4@users.noreply.github.com>
2022-10-21 01:34:49 -0700

    can now upload a csv file to make a board

--- trebek_client/src/components/Board.js
+++ trebek_client/src/components/Board.js
@@ -1,4 +1,5 @@
 import { React, useState } from 'react';
+import { stringify } from 'uuid';
 import Column from './Column';
 
 
@@ -11,7 +12,11 @@ export default function Board() {
         justifyContent: 'space-evenly'
     }
     const [selectedFile, setSelectedFile] = useState();
-    const [isFilePicked, setIsFilePicked] = useState(false)
+    const [isFilePicked, setIsFilePicked] = useState(false);
+    const [columnsV3, setColumnsV3] = useState('');
+    const [importBoard, setImportBoard] = useState([]);
+
+    const reader = new FileReader();
 
     // const questionJSON = {
     //     "questions": [
@@ -32,7 +37,7 @@ export default function Board() {
     //     </div>
     // )
 
-    const boardJSON = {
+    const oldBoardJSON = {
         'board': [
             {'title': 'Space', 'questions': '["q1", "q2", "q3", "q4", "q5"]', 'points': '["200","400","600","800","1000"]' },
             {'title': 'Famous Quotes', 'questions': '["q1", "q2", "q3", "q4", "q5"]', 'points': '["200","400","600","800","1000"]' },
@@ -41,6 +46,10 @@ export default function Board() {
             {'title': 'O Canada', 'questions': '["q1", "q2", "q3", "q4", "q5"]', 'points': '["200","400","600","800","1000"]' },
             {'title': '???', 'questions': '["q1", "q2", "q3", "q4", "q5"]', 'points': '["200","400","600","800","1000"]' }
         ]
+    }
+
+    let boardJSON = {
+        'board': []
     }
 
     const boardJSONEmpty = {
@@ -56,6 +65,7 @@ export default function Board() {
 
     const changeHandler = (event) => {
         setSelectedFile(event.target.files[0]);
+        reader.readAsText(event.target.files[0])
         setIsFilePicked(true);
     }
 
@@ -63,25 +73,71 @@ export default function Board() {
         console.log(selectedFile);
     };
 
+    var boardArr = []
+
+    reader.addEventListener('load', () => {
+        var arr = reader.result.split("\r\n")
+        var qArr = []
+        for (let i = 0; i < arr.length; i++) {
+            let temp = arr[i].split(",")
+            qArr.push(temp)
+        }
+
+        for (let j = 0; j < 6; j++) {
+            let temp = []
+            qArr.forEach(element => {
+                temp.push(element[j])
+            });
+            boardArr.push(temp)
+        }
+
+
+        boardArr.forEach(element => {
+            let qArrString = JSON.stringify(element.slice(1,7))
+            let prep = JSON.stringify(qArrString)
+            console.log(prep)
+            let temp = {'title': element[0], 'questions': qArrString, 'points': '["200","400","600","800","1000"]'}
+            boardJSON.board.push(temp);
+        })
+
+        setColumnsV3(boardJSON.board.map((topic) => 
+        <li key = {topic.title}style = {{float: 'left', listStyle:'none'}}>{<Column {...topic}/>}</li>
+        ))
+        setIsFilePicked(true)
+        
+        // setImportBoard(JSON.stringify(reader.result))
+    })
+
     let boardObj = boardJSONEmpty.board
 
     let questionsAndPoints
 
-    const columnsV2 = boardJSONEmpty.board.map((topic) => 
+    const columnsV2 = boardJSON.board.map((topic) => 
     <li key = {topic.title}style = {{float: 'left', listStyle:'none'}}>{<Column {...topic}/>}</li>
     )
 
+    
+
     // {...questionsAndPoints = {title: topic.title, questions: topic.questions, points: topic.points}}
 
-
+    //accept="application/vnd.ms/excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
 
     return (
         <div style={styles}>
+        {!isFilePicked &&
         <div>
-            <input type = 'file' name='file' onChange={changeHandler} />
-            <button onClick={handleSubmission}>submit</button>
+            <div>
+            {reader.result}
+            </div>
+            <input type = 'file' name='file'  onChange={changeHandler} />
+            {/* <button onClick={handleSubmission}>submit</button> */}
         </div>
-        {/* <ul>{columnsV2}</ul> */}
+            }            
+        {isFilePicked &&
+            <>
+                <ul>{columnsV3}</ul>
+            </>
+        }
         </div>
     );
 }
