99b46f92473cd6049ead6f804c5c8cea40fd4313
C14H14N2O4 <99705187+C14H14N2O4@users.noreply.github.com>
2022-10-21 01:35:36 -0700

    all changes

---
+++ server2/server2.js
@@ -0,0 +1,15 @@
+import { Server } from "socket.io";
+
+const io = new Server(3000);
+
+console.log('server spun up')
+
+io.on("connection", (socket) => {
+  // send a message to the client
+  socket.emit("hello from server", 1, "2", { 3: Buffer.from([4]) });
+
+  // receive a message from the client
+  socket.on("hello from client", (...args) => {
+    // ...
+  });
+});
